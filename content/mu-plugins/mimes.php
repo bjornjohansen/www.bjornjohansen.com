635d9a3f6dc82ebbb89f85ef8869e94fa11f2388
Bjørn Johansen <bjorn@dekode.no>
2017-12-13 05:23:09 +0100

    Populate the upload mimes with some useful data.

---
+++ content/mu-plugins/mimes.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Populate the upload mimes with some useful data.
+ *
+ * @package bjornjohansen\bjornjohansen.no
+ */
+
+/**
+ * Populate the upload mimes with some useful data.
+ *
+ * @param  array $mimes Current mimes.
+ * @return array        Modified mimes.
+ */
+function bj_populate_mimes( $mimes ) {
+		$mime_types = array(
+			'ac3' => 'audio/ac3',
+			'mpa' => 'audio/MPA',
+			'flv' => 'video/x-flv',
+			'ai'  => 'application/postscript',
+			'eps' => 'application/postscript',
+			'ppt' => 'application/vnd.ms-powerpoint',
+			'pps' => 'application/vnd.ms-powerpoint',
+			'svg' => 'image/svg+xml',
+		);
+		return array_merge( $mimes, $mime_types );
+}
+add_filter( 'upload_mimes', 'bj_populate_mimes' );
