2654bde823836bb411452ee907b6ab26ffcb248e
Bjørn Johansen <bjorn@dekode.no>
2017-12-13 05:14:21 +0100

    Add immutable-assets mu-plugin

---
+++ content/mu-plugins/immutable-assets.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Create immutable asset URLs.
+ *
+ * @package Immutable_Assets
+ * @author bjornjohansen
+ * @version 0.1.0
+ * @license https://www.gnu.org/licenses/old-licenses/gpl-2.0.html  GNU General Public License version 2 (GPLv2)
+ */
+
+/**
+ * Load the immutable assets feature.
+ */
+require __DIR__ . '/immutable-assets/immutable-assets.php';
---
+++ content/mu-plugins/immutable-assets/immutable-assets.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Create immutable asset URLs.
+ *
+ * @package Immutable_Assets
+ * @author bjornjohansen
+ * @version 0.1.0
+ * @license https://www.gnu.org/licenses/old-licenses/gpl-2.0.html  GNU General Public License version 2 (GPLv2)
+ */
+
+/*
+You want something like this in your Nginx config server context:
+location ~* (.+)\.(?:[a-zA-Z0-9]+)\.(js|css)$ {
+	try_files $uri $1.$2;
+	expires max;
+	add_header Cache-Control "public, immutable";
+}
+
+or something like this in your Apache VirtualHost section (or .htaccess):
+<IfModule mod_rewrite.c>
+	RewriteEngine On
+	RewriteCond %{REQUEST_FILENAME} !-f
+	RewriteRule (.+)\.(?:[a-zA-Z0-9]+)\.(js|css)$ $1.$2 [L]
+</IfModule>
+<IfModule mod_headers.c>
+	<FilesMatch "\.(js|css)$">
+		Header set Expires "Thu, 31 Dec 2037 23:55:55 GMT"
+		Header set Cache-Control "max-age=315360000, public, immutable"
+	</FilesMatch>
+</IfModule>
+*/
+
+require __DIR__ . '/immutable-assets/class-immutable-assets.php';
+
+add_action(
+	'init', function() {
+		$ia = new Immutable_Assets();
+		$ia->wp_setup();
+	}
+);
