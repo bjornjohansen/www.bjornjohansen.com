e8542d1b93aed4f5e3e0f0c68730d0b4c2183143
Bjørn Johansen <bjorn@dekode.no>
2018-01-25 19:05:37 +0100

    Add a notice for WordCamp Oslo 2018 to the start and end of each post.

--- content/mu-plugins/bjornad.php
+++ content/mu-plugins/bjornad.php
@@ -5,6 +5,8 @@
  * @package BJ\Ads
  */
 
+namespace BJ\Ads;
+
 /**
  * The bjornad shortcode callback handler.
  *
@@ -14,4 +16,25 @@
 function bjornad_func( $atts ) {
 	return '';
 }
-add_shortcode( 'bjornad', 'bjornad_func' );
+add_shortcode( 'bjornad', '\BJ\Ads\bjornad_func' );
+
+
+/**
+ * Add an ad to the start and end of each post.
+ *
+ * @param string $content The post content.
+ * @return string The post content with ads.
+ */
+function display_ad( $content ) {
+
+	if ( is_single() && 2124 !== get_the_ID() ) {
+
+		$ad_content = '<hr><p>By the way: <a href="https://bjornjohansen.no/welcome-to-wordcamp-oslo-2018">Check out the WordPress conference <span style="white-space:nowrap">WordCamp Oslo 2018</span></a></p><hr>';
+
+		$content = $ad_content . $content . $ad_content;
+	}
+
+	return $content;
+
+}
+add_filter( 'the_content', '\BJ\Ads\display_ad' );
