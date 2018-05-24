b8c6c66175350793b091cc42be03f748a8ec19ba
Bjørn Johansen <bjorn@dekode.no>
2018-05-24 21:30:05 +0200

    Add a Servebolt advertisement right after the “read more” separator.

--- content/mu-plugins/bjornad.php
+++ content/mu-plugins/bjornad.php
@@ -38,3 +38,23 @@ function display_ad( $content ) {
 
 }
 add_filter( 'the_content', '\BJ\Ads\display_ad' );
+
+
+/**
+ * Add an advertisement right after the “read more” separator.
+ */
+add_filter(
+	'the_content', function ( $content ) {
+		if ( ! is_singular() ) {
+			return $content;
+		}
+
+		$ad = '<div style="border-top: 1px solid #aaa; border-bottom: 1px solid #aaa;"><div style="font-size: 0.75em; color: #aaa;">Advertisement:</div><div style="text-align: center"><a href="https://servebolt.com/platforms/wordpress/?ref=bjornjohansen" rel="nofollow"><img src="https://bjornjohansen.no/content/uploads/2018/05/servebolt-ad.png" border="0"></a></div></div>';
+
+		if ( false !== strpos( $content, '<!--more-->' ) ) {
+			$content = str_replace( '<!--more-->', '<!--more-->' . $ad, $content );
+		}
+
+		return $content;
+	}, 10, 1
+);
