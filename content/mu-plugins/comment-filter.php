b9cd5edc48d9d620f354d6be4723e1d6baccc912
Bjørn Johansen <post@bjornjohansen.no>
2018-08-21 12:04:08 +0200

    Tidy up the comment filter.

--- content/mu-plugins/comment-filter.php
+++ content/mu-plugins/comment-filter.php
@@ -19,17 +19,15 @@ namespace BJ\Comment;
  */
 function comment_filter_cyrillic( $approved, $commentdata ) {
 
-	$comment_content = $commentdata['comment_content'];
-
 	// Remove all whitespaces.
-	$input = preg_replace( '/\s/', '', $comment_content );
-
-	$orgin_len = mb_strlen( $input );
+	$input = preg_replace( '/\s/', '', $commentdata['comment_content'] );
 
 	// Remove all cyrillic letters (U+0400..U+04FF).
 	$filtered_input = mb_ereg_replace( '[Ѐ-ӿ]', '', $input );
-	$filtered_len   = mb_strlen( $filtered_input );
 
+	// Calculate the ratio of cyrillic in the input.
+	$orgin_len      = mb_strlen( $input );
+	$filtered_len   = mb_strlen( $filtered_input );
 	$ratio_filtered = ( $orgin_len - $filtered_len ) / $orgin_len;
 
 	// If more than 50% is cyrillic, it is spam.
